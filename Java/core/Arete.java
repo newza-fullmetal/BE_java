8969471d04003d82a3fc772aa25bbe97dcb29e95
flolight <clanet@etud.insa-toulouse.fr>
2016-03-21 18:34:16 +0100

    arete

--- Java/core/Arete.java
+++ Java/core/Arete.java
@@ -1,5 +1,35 @@
 package core;
 
-public class Arete {
+import base.Descripteur;
 
+public class Arete {
+	/** Le noeud de départ */
+	private Noeud depart;
+	
+	/** Le Noeud d'arrivée */
+	private Noeud arrivee;
+	
+	/** La description de la route */
+	private Descripteur desc;
+	
+	
+	/** Le constructeur */
+	
+	public Arete(Noeud a, Noeud b, Descripteur desc){
+		this.depart = a;
+		this.arrivee = b;
+		this.desc = desc;
+		
+		if(desc.isSensUnique()){
+			this.depart.addSuiv();
+		}else{
+			
+		}
+	}
+	
+	public Noeud getDepart(){return this.depart;}
+	
+	public Noeud getArrivee(){return this.arrivee;}
+	
+	public Descripteur getDescripteur(){return this.desc;}
 }
